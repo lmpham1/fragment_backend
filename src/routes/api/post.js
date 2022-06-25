737d901e0886f7c23bddd5c827595e52133cabfb
Minh Pham <est2000vn@gmail.com>
2022-06-25 03:03:16 -0400

    fixed status code in post.js

--- src/routes/api/post.js
+++ src/routes/api/post.js
@@ -7,20 +7,18 @@ const { createSuccessResponse, createErrorResponse } = require('../../response')
  * Get a list of fragments for the current user
  */
 module.exports = async (req, res) => {
-  try {
-    const fragmentValue = req.body;
-    const fragment = new Fragment({ ownerId: '1234', type: req.headers['content-type'] });
-    if (!Fragment.isSupportedType(fragment.type)) {
-      throw new Error(`data of type ${fragment.type} is unsupported!`);
-    }
-    await fragment.save();
-    await fragment.setData(fragmentValue);
-    // TODO: this is just a placeholder to get something working...
+  const fragmentValue = req.body;
+  const fragment = new Fragment({ ownerId: req.user, type: req.headers['content-type'] });
+  if (!Fragment.isSupportedType(fragment.type)) {
     res
-      .status(200)
-      .location(process.env.API_URL + '/' + fragment.id)
-      .json(createSuccessResponse(fragment));
-  } catch (err) {
-    res.status(415).json(createErrorResponse(415, err));
+      .status(415)
+      .json(createErrorResponse(415, `Data of type ${fragment.type} is not supported`));
   }
+  await fragment.save();
+  await fragment.setData(fragmentValue);
+  // TODO: this is just a placeholder to get something working...
+  res
+    .status(201)
+    .location(process.env.API_URL + '/' + fragment.id)
+    .json(createSuccessResponse(fragment));
 };
