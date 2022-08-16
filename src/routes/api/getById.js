b901169b519a2fd845ae7c221e046f88cf662012
Minh Pham <est2000vn@gmail.com>
2022-08-16 16:18:10 -0400

    added support for file conversion

--- src/routes/api/getById.js
+++ src/routes/api/getById.js
@@ -1,4 +1,5 @@
-// src/routes/api/get.js
+// src/routes/api/getById.js
+const sharp = require('sharp');
 
 const { Fragment } = require('../../model/fragment');
 const { createErrorResponse } = require('../../response');
@@ -7,11 +8,10 @@ const md = require('markdown-it')();
  * Get a list of fragments for the current user
  */
 module.exports = async (req, res) => {
-  let id = req.params.id;
-  let isMarkdown = false;
-  if (id.endsWith('.md')) {
-    id = id.substr(0, id.search(/\.[A-Za-z]+$/));
-    isMarkdown = true;
+  const url = req.params.id;
+  let id = url;
+  if (url.match(/^[A-Za-z0-9_-]+\.[A-Za-z]+$/)) {
+    id = url.substr(0, url.search(/\.[A-Za-z]+$/));
   }
   try {
     const fragment = await Fragment.byId(req.user, id);
@@ -24,16 +24,31 @@ module.exports = async (req, res) => {
         charset = fragment.type.substr(fragment.type.search(/(?<=charset=).*$/g));
       }
       data = data.toString(charset);
-      if (isMarkdown) {
+      if (url.endsWith('.md')) {
         data = md.render(data);
       }
-
       res.status(200).setHeader('Content-Type', fragment.type).send(data);
     } else if (mimeType === 'application/json') {
       data = JSON.parse(data.toString());
       res.status(200).setHeader('Content-Type', fragment.type).json(data);
     } else if (mimeType.startsWith('image/')) {
-      res.status(200).setHeader('Content-Type', fragment.type).send(data);
+      data = sharp(data);
+      let viewType = fragment.type;
+      if (url.endsWith('.jpeg') || id.endsWith('.jpg')) {
+        data = await data.jpeg();
+        viewType = 'image/jpeg';
+      } else if (url.endsWith('.png')) {
+        data = await data.png();
+        viewType = 'image/png';
+      } else if (url.endsWith('.webp')) {
+        data = await data.webp();
+        viewType = 'image/webp';
+      } else if (url.endsWith('.gif')) {
+        data = await data.gif();
+        viewType = 'image/gif';
+      }
+      data = await data.toBuffer();
+      res.status(200).setHeader('Content-Type', viewType).send(data);
     }
   } catch (err) {
     console.log(err);
